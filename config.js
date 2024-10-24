452ed80abec4867fb50b7c912abdad52db07d064
D4v31x <davidbrichacek28@gmail.com>
2024-10-24 22:02:36 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    appwriteEndpoint: 'YOUR_APPWRITE_ENDPOINT',
+    appwriteProjectId: 'YOUR_APPWRITE_PROJECT_ID',
+    appwriteDatabaseId: 'blog_database',
+    appwriteCollectionId: 'posts',
+    appwriteTeamId: 'developers_team' // Team ID for developers
+  };
+  
